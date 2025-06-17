e72cde3228c1c7fb933f78e0916f76b51104ecd6
AnastasiaBndr <abondarchuk1149@gmail.com>
2025-06-17 13:46:57 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -52,40 +52,46 @@ function fetchWithTimeout(page) {
         lightbox.refresh();
     }
     loadMoreButton.classList.remove('visible');
-    setTimeout(function () {
-        pixabayApi(input.value, page).then(posts => {
-            if (posts.hits.length === 0)
-                throw new Error("Sorry, there are no images matching your search query. Please try again!");
-            let top = 0;
-            if (page > 1) {
-                const rect = gallery.firstChild.getBoundingClientRect();
-                top = rect.height * 2;
-            }
+    setTimeout(async function () {
+        try {
+            const posts = await pixabayApi(input.value, page);
+                if (posts.hits.length === 0)
+                    throw new Error("Sorry, there are no images matching your search query. Please try again!");
+                let top = 0;
+                if (page > 1) {
+                    const rect = gallery.firstChild.getBoundingClientRect();
+                    top = rect.height * 2;
+                }
 
-            gallery.insertAdjacentHTML("beforeend", renderFunction(posts.hits));
-            window.scrollBy({ top, behavior: "smooth" });
-            lightbox = new SimpleLightbox('.gallery a', {
-                captions: true,
-                captionSelector: 'img',
-                captionPosition: 'outside',
-                captionDelay: 250,
-                overlayOpacity: 0.8,
-                styles: '../css/styles.css',
-                captionsData: 'alt'
-            }
-            )
-            if ((posts.total - posts.hits.length * page) > posts.hits.length) {
-                loadMoreButton.classList.add('visible');
-            } else {
-                loadMoreButton.classList.remove('visible');
-                iziToast.info({
-                    message: "We're sorry, but you've reached the end of search results.", iconColor,
-                    maxWidth, closeOnClick, position, displayMode, progressBar,
-                })
-            }
-        }).catch((err) => iziToast.error({
+                gallery.insertAdjacentHTML("beforeend", renderFunction(posts.hits));
+                window.scrollBy({ top, behavior: "smooth" });
+                lightbox = new SimpleLightbox('.gallery a', {
+                    captions: true,
+                    captionSelector: 'img',
+                    captionPosition: 'outside',
+                    captionDelay: 250,
+                    overlayOpacity: 0.8,
+                    styles: '../css/styles.css',
+                    captionsData: 'alt'
+                }
+                )
+                if ((posts.total - posts.hits.length * page) > posts.hits.length) {
+                    loadMoreButton.classList.add('visible');
+                } else {
+                    loadMoreButton.classList.remove('visible');
+                    iziToast.info({
+                        message: "We're sorry, but you've reached the end of search results.", iconColor,
+                        maxWidth, closeOnClick, position, displayMode, progressBar,
+                    })
+                }
+        } catch (err) {
+            iziToast.error({
             message: `${err.message ? err.message : err}`, closeOnClick: true, position,
             displayMode, progressBar, backgroundColor, messageColor, iconColor, maxWidth,
-        })).finally(loader.classList.remove('visible'))
+        })
+        } finally {
+            loader.classList.remove('visible')
+        }
+        
     }, 1000);
 }
